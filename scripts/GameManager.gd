50ed7dc16cd55769e5c6bc86ef7067eb083e48c9
Pablo Delgado de Mendoza <pablodelgado03@gmail.com>
2026-05-13 12:56:27 +0200

    Charts added to rooms

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -1,5 +1,14 @@
 extends Node
 
-var animals_room_fixed: bool = false
-var landscape_room_fixed: bool = false
-var flora_room_fixed: bool = false
+# Animales
+var puzzle_animal_1 = false
+var puzzle_animal_2 = false
+var puzzle_animal_3 = false
+
+# Flora
+var puzzle_flora_1 = false
+var puzzle_flora_2 = false
+
+# Paisajes
+var puzzle_landscape_1 = false
+var puzzle_landscape_2 = false

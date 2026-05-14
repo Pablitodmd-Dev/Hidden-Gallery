f8e779c66388d626fe72a2ec40e6b3689392d3e8
fersaezl <saezlazaro11@gmail.com>
2026-05-14 10:51:37 +0200

    Finish Controls interface

--- scripts/Ui/main_menu.gd
+++ scripts/Ui/main_menu.gd
@@ -1,10 +1,16 @@
 extends Control
 
 func _on_play_pressed() -> void:
+	$Pop.play()
+	await $Pop.finished
 	get_tree().change_scene_to_file("res://scenes/Rooms/PrincipalRoom.tscn")
 
 func _on_controls_pressed() -> void:
+	$Pop.play()
+	await $Pop.finished
 	get_tree().change_scene_to_file("res://scenes/ui/Controls.tscn")
 
 func _on_exit_pressed() -> void:
+	$Pop.play()
+	await $Pop.finished
 	get_tree().quit()

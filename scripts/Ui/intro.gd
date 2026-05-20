9a5036beb61c35fed4da6efbe29a2522f247bf57
fersaezl <saezlazaro11@gmail.com>
2026-05-20 08:13:38 +0200

    Add the Dialogic 2 plugin, implement the dialogue and add some voices

---
+++ scripts/Ui/intro.gd
@@ -0,0 +1,14 @@
+extends Control
+
+func _ready():
+	MusicPlayer.stop()
+	BgMusic.stop()
+	Dialogic.start("intro")
+	
+	Dialogic.signal_event.connect(_on_dialogic_signal)
+	Dialogic.start("intro")
+
+func _on_dialogic_signal(argument):
+	if argument == "intro_finished":
+		get_tree().change_scene_to_file("res://scenes/ui/MainMenu.tscn")
+		MusicPlayer.play()

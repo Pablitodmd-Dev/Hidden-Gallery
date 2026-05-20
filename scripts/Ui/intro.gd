cbc49da8c6ef8da562ba76b20a5e69ed33793729
fersaezl <saezlazaro11@gmail.com>
2026-05-20 14:17:00 +0200

    Add rain sound and configure it in intro.tscn

--- scripts/Ui/intro.gd
+++ scripts/Ui/intro.gd
@@ -1,11 +1,17 @@
 extends Control
 
+@onready var rain = $Rain
+
 func _ready():
 	MusicPlayer.stop()
+	
+	rain.play()
+	
 	Dialogic.signal_event.connect(_on_dialogic_signal)
 	Dialogic.start("intro")
 
 func _on_dialogic_signal(argument):
 	if argument == "intro_finished":
 		get_tree().change_scene_to_file("res://scenes/ui/MainMenu.tscn")
+		rain.stop()
 		MusicPlayer.play()

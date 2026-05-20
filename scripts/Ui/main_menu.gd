db4ea0945227cd753205a08bc9737311edd0913f
Pablo Delgado de Mendoza <pablodelgado03@gmail.com>
2026-05-20 11:07:49 +0200

    Animation to main menu created and icon game designed and implemented

--- scripts/Ui/main_menu.gd
+++ scripts/Ui/main_menu.gd
@@ -1,5 +1,8 @@
 extends Control
 
+@onready var anim_player = $AnimationPlayer
+@onready var color_rect = $ColorRect
+
 func _ready() -> void:
 	var backgroundMusic = load("res://assets/Sounds/bgMusic/RelaxingPiano.mp3")
 	
@@ -7,6 +10,12 @@ func _ready() -> void:
 		MusicPlayer.stream = backgroundMusic
 		MusicPlayer.volume_db = -25.0
 		MusicPlayer.play()
+
+	color_rect.visible = true
+	if anim_player.has_animation("fade_in"):
+		anim_player.play("fade_in")
+		await anim_player.animation_finished
+	color_rect.visible = false
 
 func _on_play_pressed() -> void:
 	$Pop.play()
